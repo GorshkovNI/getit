113c35d07a8f8457b21a34d6ac00074140eac2cf
Gorshkov Nikita <nigorshkov99@gmail.com>
2023-06-19 15:00:47 +0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const App = () => {
+    const num = {a: 'string'}
+    return (
+        <h1>Hello world {num.a}</h1>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+console.log(mode + ' mode')
+
+
+module.exports = {
+    mode: mode,
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "js/main.js",
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@view': path.resolve(__dirname, 'src/views')
+        }
+    },
+
+    externals: ['.js', '.ts', '.jsx', '.tsx'],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x$/,
+                exclude: /node_module/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            // Options
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/public/index.html"
+        }),
+        new ForkTsCheckerWebpackPlugin()
+    ],
+
+    devServer: {
+        port: 8000,
+        static: './dist'
+    },
+
+    devtool: 'source-map'
+}
